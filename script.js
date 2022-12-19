6531597ef373082a6f988910a1814636fb160726
Ryan Ramboer <ryanramboer@gmail.com>
2022-12-18 21:19:37 -0500

    add dark mode stub/formatting

--- script.js
+++ script.js
@@ -1,67 +1,71 @@
-
 Vue.createApp({
-  data() {
-    return {
-      recipes: [
-        {
-          title: "Avocado Toast",
-          image: "images/temp1.jpeg",
-        },
-        {
-          title: "Chocolate Chip Pancakes",
-          image: "images/temp2.png",
-        },
-        {
-          title: "Avocado Toast",
-          image: "images/temp3.jpeg",
-        },
-        {
-          title: "Avocado Toast",
-          image: "images/temp3.jpeg",
-        },
-      ],
-      categories: [
-        {
-          name: "Entrées",
-          image: "category_images/placeholder.png",
-          toggle: null,
-        },
-        {
-          name: "Appetizers",
-          image: "category_images/placeholder.png",
-          toggle: null,
-        },
-        {
-          name: "Breakfast",
-          image: "category_images/placeholder.png",
-          toggle: null,
-        },
-        {
-          name: "Desserts",
-          image: "category_images/placeholder.png",
-          toggle: null,
-        },
-        {
-          name: "Soup & Sauces",
-          image: "category_images/placeholder.png",
-          toggle: null,
-        },
-        {
-          name: "Crockpot",
-          image: "category_images/placeholder.png",
-          toggle: null,
-        },
-        {
-          name: "Drinks",
-          image: "category_images/placeholder.png",
-          toggle: null,
-        },
-        {
-          name: "Miscellaneous",
-          image: "category_images/placeholder.png",
-          toggle: null,
-        },
-      ],
-    };
-  }
+    data() {
+        return {
+            recipes: [
+                {
+                    title: "Avocado Toast",
+                    image: "images/temp1.jpeg",
+                },
+                {
+                    title: "Chocolate Chip Pancakes",
+                    image: "images/temp2.png",
+                },
+                {
+                    title: "Avocado Toast",
+                    image: "images/temp3.jpeg",
+                },
+                {
+                    title: "Avocado Toast",
+                    image: "images/temp3.jpeg",
+                },
+            ],
+            categories: [
+                {
+                    name: "Entrées",
+                    image: "category_images/placeholder.png",
+                    toggle: null,
+                },
+                {
+                    name: "Appetizers",
+                    image: "category_images/placeholder.png",
+                    toggle: null,
+                },
+                {
+                    name: "Breakfast",
+                    image: "category_images/placeholder.png",
+                    toggle: null,
+                },
+                {
+                    name: "Desserts",
+                    image: "category_images/placeholder.png",
+                    toggle: null,
+                },
+                {
+                    name: "Soup & Sauces",
+                    image: "category_images/placeholder.png",
+                    toggle: null,
+                },
+                {
+                    name: "Crockpot",
+                    image: "category_images/placeholder.png",
+                    toggle: null,
+                },
+                {
+                    name: "Drinks",
+                    image: "category_images/placeholder.png",
+                    toggle: null,
+                },
+                {
+                    name: "Miscellaneous",
+                    image: "category_images/placeholder.png",
+                    toggle: null,
+                },
+            ],
+        };
+    },
+    methods: {
+        toggleDarkMode() {
+
+        }
+    }
 }).mount("#app");
