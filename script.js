1dcdce99f0e185e6e7e9b4e2933e3473ec832d57
Khuldraeseth <28711596+khuldraeseth@users.noreply.github.com>
2022-12-18 23:31:02 -0500

    Revert "Use the lighterweight category images"
    This reverts commit 12a2e31e3b05eaa5f2d2bc7006d092433a7ce609.

--- script.js
+++ script.js
@@ -22,42 +22,42 @@ Vue.createApp({
             categories: [
                 {
                     name: "Entrées",
-                    image: "category_images/green_chicken_1080p.jpg",
+                    image: "category_images/green_chicken.jpg",
                     toggle: null,
                 },
                 {
                     name: "Appetizers",
-                    image: "category_images/garlic_rosemary_1080p.jpg",
+                    image: "category_images/garlic_rosemary.jpg",
                     toggle: null,
                 },
                 {
                     name: "Breakfast",
-                    image: "category_images/breakfast_bowl_1080p.png",
+                    image: "category_images/breakfast_bowl.png",
                     toggle: null,
                 },
                 {
                     name: "Desserts",
-                    image: "category_images/oreo_cookies_1080p.jpg",
+                    image: "category_images/oreo_cookies.jpg",
                     toggle: null,
                 },
                 {
                     name: "Soup & Sauces",
-                    image: "category_images/broccoli_fennel_soup_1080p.jpg",
+                    image: "category_images/broccoli_fennel_soup.jpg",
                     toggle: null,
                 },
                 {
                     name: "Crockpot",
-                    image: "category_images/meatloaf_1080p.jpg",
+                    image: "category_images/meatloaf.jpg",
                     toggle: null,
                 },
                 {
                     name: "Drinks",
-                    image: "category_images/mango_mint_1080p.jpg",
+                    image: "category_images/mango_mint.jpg",
                     toggle: null,
                 },
                 {
                     name: "Miscellaneous",
-                    image: "category_images/paleo_bread_1080p.jpg",
+                    image: "category_images/paleo_bread.jpg",
                     toggle: null,
                 },
             ],
