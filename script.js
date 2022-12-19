a6e807102a8af5a35ae4081461b201d56b299d20
Ryan Ramboer <ryanramboer@gmail.com>
2022-12-18 22:28:28 -0500

    add all category images

--- script.js
+++ script.js
@@ -22,42 +22,42 @@ Vue.createApp({
             categories: [
                 {
                     name: "Entrées",
-                    image: "category_images/placeholder.png",
+                    image: "category_images/green_chicken.jpg",
                     toggle: null,
                 },
                 {
                     name: "Appetizers",
-                    image: "category_images/placeholder.png",
+                    image: "category_images/garlic_rosemary.jpg",
                     toggle: null,
                 },
                 {
                     name: "Breakfast",
-                    image: "category_images/placeholder.png",
+                    image: "category_images/breakfast_bowl.png",
                     toggle: null,
                 },
                 {
                     name: "Desserts",
-                    image: "category_images/placeholder.png",
+                    image: "category_images/oreo_cookies.jpg",
                     toggle: null,
                 },
                 {
                     name: "Soup & Sauces",
-                    image: "category_images/placeholder.png",
+                    image: "category_images/broccoli_fennel_soup.jpg",
                     toggle: null,
                 },
                 {
                     name: "Crockpot",
-                    image: "category_images/placeholder.png",
+                    image: "category_images/meatloaf.jpg",
                     toggle: null,
                 },
                 {
                     name: "Drinks",
-                    image: "category_images/placeholder.png",
+                    image: "category_images/mango_mint.jpg",
                     toggle: null,
                 },
                 {
                     name: "Miscellaneous",
-                    image: "category_images/placeholder.png",
+                    image: "category_images/paleo_bread.jpg",
                     toggle: null,
                 },
             ],
@@ -68,13 +68,13 @@ Vue.createApp({
 let darkModeFlag = false; // false = light mode, true = dark mode
 
 function toggleDarkMode() {
-    var element = document.body;
+    let root = document.documentElement;
     if (darkModeFlag) { // if currently is dark mode, switch to light mode
-        element.classList.remove("dark-mode");
+        root.style.setProperty(); // TODO: do this a bunch of times
         darkModeFlag = false;
     }
     else { // if currently is light mode, switch to dark mode
-        element.classList.add("dark-mode");
+        root.style.setProperty();
         darkModeFlag = true;
     }
 }
